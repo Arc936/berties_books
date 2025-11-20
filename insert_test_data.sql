36c5eee701254f8c33ebc210747207ccc2455022
arc936 <blabb3337@gmail.com>
2025-11-20 22:29:06 +0000

    s

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,4 +3,4 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
-INSERT INTO users (username, firstName, lastName, email, hashedPassword) VALUES ('gold','gold','smiths','placeholder@gmail.com','$2b$10$5tu9sR.m0O4HwwiU0p64yueb7IRgfc7gHMkixy0dm7PWWsFUbSa5C')
+INSERT INTO users (username, firstName, lastName, email, hashedPassword) VALUES ('gold','gold','smiths','placeholder@gmail.com','$2b$10$5tu9sR.m0O4HwwiU0p64yueb7IRgfc7gHMkixy0dm7PWWsFUbSa5C');
